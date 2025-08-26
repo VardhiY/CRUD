cb1d4733b4c91548fb771f6dd3b405ef4908953e
VardhiY <vardhinivaruy123@gmail.com>
2025-08-26 21:14:05 +0530

    Fix Vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": "http://localhost:5000"
-    }
-  }
+    port: 5173,
+  },
+  base: "./", // ✅ Important for Vercel
 });
